c68cb5a719b14183c3dc6da8f115b80054e429ce
FulllMental <104234625+FulllMental@users.noreply.github.com>
2022-11-14 20:00:45 +0300

    Add default values for variable

--- project/settings.py
+++ project/settings.py
@@ -12,11 +12,11 @@ INSTALLED_APPS = ['datacenter']
 
 SECRET_KEY = env.str('SECRET_KEY')
 
-DEBUG = env.bool("DEBUG")
+DEBUG = env.bool("DEBUG", False)
 
 ROOT_URLCONF = 'project.urls'
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', 'localhost')
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES = [
